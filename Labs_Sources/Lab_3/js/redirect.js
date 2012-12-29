56f5b40c70886a88d27dd9124fcf65ba67d70a75
Sergey Rudenko <awbi91@gmail.com>
2012-12-30 00:20:36 +0200

    Lab 3 scripts complete
    Add validation with jquery and animation

---
+++ Labs_Sources/Lab_3/js/redirect.js
@@ -0,0 +1,19 @@
+﻿//counter for redirect page
+var timerCount = 3;
+//Main redirect function
+function FuncRedirect()
+{
+	location.href="index.html";
+}
+//Counter changes for redirect 
+function TimeChange()
+{
+	timerCount--;
+	document.getElementById("timeVal").innerHTML = timerCount;
+}
+//Main Delay function for redirect page
+function FuncDelay()
+{
+	setTimeout("FuncRedirect()", 3000);
+	setInterval("TimeChange()", 1000);
+}
--- Labs_Sources/Lab_3/js/valid.js
+++ Labs_Sources/Lab_3/js/valid.js
@@ -1,25 +1,76 @@
-//TODO: Refactor valid function and add color changes when not valid textboxes
+//New OK JQuery Validation
+$(document).ready(function() {
+						   
+	$.validator.addMethod("validcb1", function(value){
+		if ($("input:checked").length < 4) return true
+		else return false;
+	},"");
+	
+	$("fieldset input").click(function(){$("fieldset label.error").remove()});
+	
+	$("#form1").validate({
+		rules: {
+			firstName: {
+				required: true,
+				minlength: 2
+				}, 
+			secondName: "required",
+			patronymic: "required",
+			checkbox7:{validcb1:true}
+			},
+		messages:{
+			firstName:{
+				required: "Поле имени не заполнено или заполнено не верно",
+				minlength: "В поле имени должно быть минимум 2 символа",
+			},
+			secondName: "Поле фамилии не заполнено или заполнено не верно",
+			patronymic: "Поле отчества не заполнено или заполнено не верно",
+			checkbox7: "Отмечено слишком много значений, максимум допускается 3!"
+		},
+		errorPlacement: function(error, element) {
+			if (element.attr("name") == "firstName") error.insertAfter($("input[name=firstName]"));
+			if (element.attr("name") == "secondName") error.insertAfter($("input[name=secondName]"));
+			if (element.attr("name") == "patronymic") error.insertAfter($("input[name=patronymic]"));
+			if (element.attr("name") == "checkbox7") error.insertAfter($("fieldset label:last"));
+		}	
+	});
+})
+
+/* Old validation
 //Valid func
 function formValidator (elem) {
-				var group = document.valform.chk_group;
-				var count = 0;
-				if (document.valform.firstName.value == "" || document.valform.firstName.value == null || 
-					document.valform.secondName.value == "" || document.valform.secondName.value == null ||
-					document.valform.patronymic.value == "" || document.valform.patronymic.value == null ) 
-				{
-						alert("Какая то формачка пустая!!11");
-						return false;
-				};
-				//Check for count of 3
-				for (var i = 0; i < group.length; i++) {
-					if(group[i].checked)
-						count++;
-				};
+	var group = document.valform.chk_group;
+	var count = 0;
 
-				if (count>3) {
-					alert("Нельзя выбирать более 3х значений!!111");
-					return false;	
-				} else {
-					return true;
-				}
-			}
+	if (document.valform.firstName.value == "" || document.valform.firstName.value == null ) 
+	{
+		alert("Форма имени пустая! Заполните её!");
+		return false;
+	};
+
+	if(document.valform.secondName.value == "" || document.valform.secondName.value == null)
+	{
+		alert("Форма фамилии пустая! Заполните её!");
+		return false;
+	};
+
+	if (document.valform.patronymic.value == "" || document.valform.patronymic.value == null)
+	{
+		alert("Форма очества пустая! Заполните её!");
+		return false;
+	};
+	
+	//Check for count of 3
+	for (var i = 0; i < group.length; i++) {
+		if(group[i].checked)
+			count++;
+	};
+
+	if (count>3) {
+		alert("Нельзя выбирать более 3х значений!!111");
+		return false;	
+	} else {
+		return true;
+	};
+}
+*/
